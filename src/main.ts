82bea63891caac726ca186879f220b042cdb762a
hamidehalaee <hamidehalaeeyasar@gmail.com>
2021-03-18 03:18:40 -0700

    add microservisec

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions  } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options:{
+        host: '127.0.0.1',
+        port: 8877,
+      },
+    }
+  );
+  app.listen(() => console.log('Microservice is listening'));
 }
 bootstrap();
+    
+
